e76e372e5bc08f352f1b8b72c6d67799b2cbaf97
Batetambe Tanyi <33326060+tanyibate@users.noreply.github.com>
2024-08-27 10:30:17 +0100

    removed unneeded components

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -3,7 +3,6 @@ import Goals from "@/components/About/Goals";
 import Hero from "@/components/About/Hero";
 import History from "@/components/About/History";
 import Intro from "@/components/About/Intro";
-import Journey from "@/components/About/Journey";
 import Values from "@/components/About/Values";
 
 const About = () => {
--- pages/donate/index.tsx
+++ pages/donate/index.tsx
@@ -1,10 +1,8 @@
-import Form from "@/components/Donate/Form";
 import Hero from "@/components/Donate/Hero";
 import Impact from "@/components/Donate/Impact";
 import Improve from "@/components/Donate/Improve";
 import Introduction from "@/components/Donate/Introduction";
 import Thankyou from "@/components/Donate/Thankyou";
-import Testimonials from "@/components/Home/Testimonials";
 
 const Donate = () => {
   return (
